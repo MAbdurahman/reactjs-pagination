5c651d29daae91cd8ef026e8fb6292b11e2d64c2
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-28 05:10:16 -0500

    Add Followers.jsx and modify App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,19 @@
+import React, { useState, useEffect } from 'react'
 import Logo from './../img/logo.svg';
-
+import { useFetch } from './../utils/useFetch';
+import paginate from './../utils/util';
+import Follower from './../components/Follower';
 
 function App() {
-  return (
+	return (
 		<div className='app'>
 			<header className='app-header'>
 				<img src={Logo} className='app-logo' alt='logo' />
 				<h1>ReactJS Pagination</h1>
 			</header>
-			<div className='app-editor'>
-
-			</div>
+			<div className='app-editor'></div>
 		</div>
-  );
+	);
 }
 
 export default App;
---
+++ src/components/Follower.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Follower() {
+	return (
+		<div>
+			<h1>This is Follower.jsx</h1>
+		</div>
+	);
+}
