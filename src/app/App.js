9fcbf11485491d1dec5d8b29d1c219b763b89bad
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-28 05:39:12 -0500

    Add keyframes for animation of loading text in index.css

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,48 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import Logo from './../img/logo.svg';
+import SpinningCircles from 'react-loading-icons';
 import { useFetch } from './../utils/useFetch';
 import paginate from './../utils/util';
 import Follower from './../components/Follower';
 
 function App() {
+	//**************** variables ****************//
+	const { loading, data } = useFetch();
+	const [page, setPage] = useState(0);
+	const [followers, setFollowers] = useState([]);
+
+	//**************** functions ****************//
 	return (
 		<div className='app'>
 			<header className='app-header'>
 				<img src={Logo} className='app-logo' alt='logo' />
 				<h1>ReactJS Pagination</h1>
 			</header>
-			<div className='app-editor'></div>
+			<main>
+				{loading ? (
+					<div className='loading'>
+						<SpinningCircles
+							height='8em'
+							width='8em'
+							fill='#003e6b'
+							stroke='#003e6b'
+							speed={1}
+							fillOpacity={1}
+							strokeWidth={2}
+							strokeOpacity={0.725}
+						/>
+						<h4 className='loading-text'> L o a d i n g . . .</h4>
+					</div>
+				) : (
+					<section className='followers'>
+						<div className='container'>
+							{followers.map(follower => {
+								return <Follower key={follower.id} {...follower} />;
+							})}
+						</div>
+					</section>
+				)}
+			</main>
 		</div>
 	);
 }
