122a51a0dd5bfe18cc89f72f53caa67ecd12734b
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-28 06:58:29 -0500

    Update styles to .followers

--- src/app/App.js
+++ src/app/App.js
@@ -12,38 +12,88 @@ function App() {
 	const [followers, setFollowers] = useState([]);
 
 	//**************** functions ****************//
+	useEffect(() => {
+		if (loading) return;
+		setFollowers(data[page]);
+	}, [loading, page, data]);
+
+	const nextPage = () => {
+		setPage(oldPage => {
+			let nextPage = oldPage + 1;
+			if (nextPage > data.length - 1) {
+				nextPage = 0;
+			}
+			return nextPage;
+		});
+	};
+	const prevPage = () => {
+		setPage(oldPage => {
+			let prevPage = oldPage - 1;
+			if (prevPage < 0) {
+				prevPage = data.length - 1;
+			}
+			return prevPage;
+		});
+	};
+
+	const handlePage = index => {
+		setPage(index);
+	};
+
+	// if (loading) {
+	// 	return (
+	// 		<div className='loading'>
+	// 			<SpinningCircles
+	// 				height='8em'
+	// 				width='8em'
+	// 				fill='#003e6b'
+	// 				stroke='#003e6b'
+	// 				speed={1}
+	// 				fillOpacity={1}
+	// 				strokeWidth={2}
+	// 				strokeOpacity={0.725}
+	// 			/>
+	// 			<h4 className='loading-text'> L o a d i n g . . .</h4>
+	// 		</div>
+	// 	);
+	// }
 	return (
-		<div className='app'>
+		<main className='app'>
 			<header className='app-header'>
 				<img src={Logo} className='app-logo' alt='logo' />
 				<h1>ReactJS Pagination</h1>
 			</header>
-			<main>
-				{loading ? (
-					<div className='loading'>
-						<SpinningCircles
-							height='8em'
-							width='8em'
-							fill='#003e6b'
-							stroke='#003e6b'
-							speed={1}
-							fillOpacity={1}
-							strokeWidth={2}
-							strokeOpacity={0.725}
-						/>
-						<h4 className='loading-text'> L o a d i n g . . .</h4>
+			<section className='followers'>
+				<div className='container'>
+					{followers.map(follower => {
+						return <Follower key={follower.id} {...follower} />;
+					})}
+				</div>
+				{!loading && (
+					<div className='btn-container'>
+						<button className='prev-btn' onClick={prevPage}>
+							prev
+						</button>
+						{data.map((item, index) => {
+							return (
+								<button
+									key={index}
+									className={`page-btn ${
+										index === page ? 'active-btn' : null
+									}`}
+									onClick={() => handlePage(index)}
+								>
+									{index + 1}
+								</button>
+							);
+						})}
+						<button className='next-btn' onClick={nextPage}>
+							next
+						</button>
 					</div>
-				) : (
-					<section className='followers'>
-						<div className='container'>
-							{followers.map(follower => {
-								return <Follower key={follower.id} {...follower} />;
-							})}
-						</div>
-					</section>
 				)}
-			</main>
-		</div>
+			</section>
+		</main>
 	);
 }
 
--- src/components/Follower.js
+++ src/components/Follower.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default function Follower() {
+export default function Follower({ avatar_url, html_url, login }) {
 	return (
-		<div>
-			<h1>This is Follower.jsx</h1>
-		</div>
+		<article className='card'>
+			<img src={avatar_url} alt={login} />
+			<h4>${login}</h4>
+			<a href={html_url} className='btn'>
+				view profile
+			</a>
+		</article>
 	);
 }
